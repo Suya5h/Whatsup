a5f19008a0bd84680c5de4742e3260f08ac5b163
Suya5h <isuyashpatel@gmail.com>
2023-05-18 12:26:02 +0530

    Back to work

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import img from "../Images/img.png";
+import Attach from "../Images/attach.png";
 
 const Input = () => {
-  return <div>Input</div>;
+  return (
+    <div className="Input">
+      <input type="text" placeholder="Type something..." />
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input type="file" style={{ display: "none" }} id="file" />
+        <label htmlFor="file">
+          <img src={img} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
