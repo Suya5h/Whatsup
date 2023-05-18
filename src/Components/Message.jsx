3f46a531bb6d0ab65a62747d0e63b21d64fe7aa6
Suya5h <isuyashpatel@gmail.com>
2023-05-18 16:31:09 +0530

    fixed the bug of message box

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -13,7 +13,7 @@ const Message = () => {
       <div className="messageContent">
         <p>Hello</p>
         <img
-          src="https://images.pexels.com/photos/16842790/pexels-photo-16842790.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          // src="https://images.pexels.com/photos/16842790/pexels-photo-16842790.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
           alt=""
         />
       </div>
