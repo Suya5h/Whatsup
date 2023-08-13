4e77eb7fabf36171fab8f064f336770cfaaee4d5
Suya5h <isuyashpatel@gmail.com>
2023-08-13 23:50:21 +0530

    installed firebase

---
+++ Backend/Firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBKVJcjmSb3LC38mob8Nn5niJCqoSF6Rpg",
+  authDomain: "whatsup-6c185.firebaseapp.com",
+  projectId: "whatsup-6c185",
+  storageBucket: "whatsup-6c185.appspot.com",
+  messagingSenderId: "786671723203",
+  appId: "1:786671723203:web:dd1fc653551b16246f7876",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
